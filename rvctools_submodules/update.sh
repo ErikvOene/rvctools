9996adfddf227a37e7d194329e733448493a77a4
Erik van Oene <erik...e@gmailcom>
2019-09-10 00:08:01 +0200

    Updated reference of the submodules and changed the update script "update.sh" for the submodules

--- rvctools_submodules/update.sh
+++ rvctools_submodules/update.sh
@@ -1,23 +1,45 @@
-echo "Updating Robot from upstream"
+# run this using ./update.sh
+echo "This will update the repos by:"
+echo " - Checkout the master branch"
+echo " - Pulling from origin"
+echo " - Pulling from upstream"
+echo " - Pusing to origin"
+echo " "
+
+echo "Updating Robot repo"
 cd robot
 git checkout master
+git pull origin master
 git pull upstream master
+git push origin master
+echo "Robot finished"
+echo " "
 
-echo "Updating Common from upstream"
+echo "Updating Common"
 cd ../common
 git checkout master
+git pull origin master
 git pull upstream master
+git push origin master
+echo "Common finished"
+echo " "
 
-echo "Updating SMTB from upstream"
+echo "Updating SMTB"
 cd ../smtb
 git checkout master
+git pull origin master
 git pull upstream master
+git push origin master
+echo "SMTB finished"
+echo " "
 
-echo "Updating Vision from upstream"
-
+echo "Updating Vision"
 cd ../vision
 git checkout master
+git pull origin master
 git pull upstream master
+git push origin master
+echo "Vision finished" 
+echo " "
 
-
-echo "Please note that a git push is still requires to update the fork!!"
+cd ..
